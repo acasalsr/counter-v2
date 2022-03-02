c167fe68fa96bf5e28b3420e3bc7d049aa257ff4
andreu casals <acasalsr@gmail.com>
2022-03-02 20:04:07 +0100

    UI + first redux structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
 
 function App() {
+  const count = useSelector((store) => store.counterReducer.count);
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    /* 
+    1 display
+    2 botons +1 +5
+    2 botons -1 -5
+    1 boto "reset"
+
+    Fer la UI
+    Pensar i afegir l’action
+    Pensar i afegir el reducer
+    Afegir el dispatch
+
+    */
+    <>
+      <div>{count}</div>
+      <button>+1</button>
+      <button>+5</button>
+      <button>-1</button>
+      <button>-5</button>
+      <button>Reset</button>
+    </>
   );
 }
 
